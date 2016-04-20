e9d68efd021d1caf2dfc29bb6efedc3beb6e04ef
RoyDu <duzhuoqi@yeah.net>
2016-04-20 11:04:34 +0800

    modified:   tK.py

--- tK.py
+++ tK.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox as messagebox
 
 class Application(Frame):
 	def __init__(self, master=None):
@@ -7,11 +8,18 @@ class Application(Frame):
 		self.createWidgets()
 
 	def createWidgets(self):
-		self.helloLabel = Label(self, text='Hello, Roy!')
+		self.nameInput = Entry(self)
+		self.nameInput.pack()
+		self.alertButton = Button(self, text='hi',command = self.hello)
+		self.alertButton.pack()
+		'''self.helloLabel = Label(self, text='Hello, Roy!')
 		self.helloLabel.pack()
 		self.quitButton = Button(self, text='Quit',command=self.quit)
-		self.quitButton.pack()
+		self.quitButton.pack()'''
 
+	def hello(self):
+		name = self.nameInput.get() or 'Roy'
+		messagebox.showinfo('Message','Hello, %s' % name)
 app = Application()
 app.master.title('hello')
 app.mainloop();
